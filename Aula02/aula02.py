ea5744d950402e81349156c2bb8299440accb948
rayssasjg <rayssajardim9@gmail.com>
2024-12-03 11:33:15 -0300

    Add files via upload
    Aprendendo sobre variável

---
+++ Aula02/aula02.py
@@ -0,0 +1,11 @@
+print('Olá, tudo bem?')
+nome_usuario='Rayssa'#tipo->str=string
+idade_usuario=18#tipo->int=integer
+altura_ususario=1.70# tipo->float=ponto flutuante
+maior_idade=True#tipo->bool=boolean
+
+print('Seja Bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_ususario, 'metros.')
+print('Você é maior de idade?', maior_idade)
+
